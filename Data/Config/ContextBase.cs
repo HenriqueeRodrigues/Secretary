e5d51d1fb166bdff6d11bb469a71a5c031915b9c
Henri <Henri@DESKTOP-CP66319>
2021-11-12 12:56:25 -0300

    Add project files.

---
+++ Data/Config/ContextBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    class ContextBase : DbContext
+    {
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public DbSet<ProdutoViewModel> ProdutoViewModel { get; set; }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Server=(localdb)\\MSSQLLocalDB;Database=Henrique;Trusted_Connection=True;";
+            return strCon;
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using Business.InterfaceProduto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : Controller
+    {
+
+        private readonly IProduto _IProduto;
+
+        public ProdutosController (IProduto IProduto)
+        {
+            _IProduto = IProduto;
+        }
+
+        [HttpGet("ListarProdutos")]
+        public async Task<IActionResult> ListarProdutos()
+        {
+            return Json(await _IProduto.List());
+        }
+
+        [HttpPost("ObterProdutoPorId")]
+        public async Task<IActionResult> ObterProdutoPorId(int Id)
+        {
+            return Json(await _IProduto.GetEntityById(Id));
+        }
+
+        [HttpPost("AdicionarProdutos")]
+        public async Task AdicionarProdutos(ProdutoViewModel produto)
+        {
+            await _IProduto.Add(produto);
+        }
+
+        [HttpPost("AtualizarProdutos")]
+        public async Task AtualizarProdutos(ProdutoViewModel produto)
+        {
+            await _IProduto.Update(produto);
+        }
+
+        [HttpPost("RemoverProdutos")]
+        public async Task RemoverProdutos(ProdutoViewModel produto)
+        {
+            await _IProduto.Delete(produto);
+        }
+    }
+}
